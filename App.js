730150c15d1a3c50e6ce62967be6ab001f7de7f6
Ahsan Khan <imkhanahsan@yahoo.com>
2021-04-13 21:39:22 +0500

    Second commit - Layout

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [getPlayer1, setPlayer1] = useState(0);
+  const [getPlayer2, setPlayer2] = useState(0);
+  const [getTurn, setTurn] = useState(1);
+
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{transform: [{rotate: '180deg'}], fontSize: 30, color: '#ffa1d0'}}>Player 2: {getPlayer2}</Text>
+
+      <View style={styles.row}>
+        <Pressable style={styles.button}>
+        </Pressable>
+        <Pressable style={styles.button}>
+        </Pressable>
+        <Pressable style={styles.button}>
+        </Pressable>
+      </View>
+
+      <View style={styles.row}>
+        <Pressable style={styles.button}>
+        </Pressable>
+        <Pressable style={styles.button}>
+        </Pressable>
+        <Pressable style={styles.button}>
+        </Pressable>
+      </View>
+
+      <View style={styles.row}>
+        <Pressable style={styles.button}>
+        </Pressable>
+        <Pressable style={styles.button}>
+        </Pressable>
+        <Pressable style={styles.button}>
+        </Pressable>
+      </View>
+      <Text style={{fontSize: 30, color: '#ffa1d0'}}>Player 1: {getPlayer1}</Text>
     </View>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#78c4d4'
   },
+  row: {
+    flexDirection: 'row',
+    height: '15%',
+    justifyContent: 'space-evenly',
+    marginVertical: 2
+  },
+  text: {
+    fontSize: 30,
+  },
+  button: {
+    backgroundColor: '#ffd880',
+    width: '25%',
+    height: '100%',
+    textAlign: 'center',
+    marginHorizontal: 2 
+  }
 });
+
+export default App;
