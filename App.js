e52483df16cf0525a2f3729e19f5a2f6cc2a5ec9
Ahsan Khan <imkhanahsan@yahoo.com>
2021-04-13 23:17:51 +0500

    Third Commit - Button Press

--- App.js
+++ App.js
@@ -1,45 +1,113 @@
 import React, { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
+const array = ["", "", "", "", "", "", "", "", ""];
 const App = () => {
   const [getPlayer1, setPlayer1] = useState(0);
   const [getPlayer2, setPlayer2] = useState(0);
   const [getTurn, setTurn] = useState(1);
 
-
-  
+  const checkTurn = (num) => {
+    if (array[num] == "") {
+      if (getTurn % 2 == 1) {
+        array[num] = 'X';
+      } else {
+        array[num] = 'O';
+      }
+    } else {
+      return;
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={{transform: [{rotate: '180deg'}], fontSize: 30, color: '#ffa1d0'}}>Player 2: {getPlayer2}</Text>
+      <Text style={{ transform: [{ rotate: '180deg' }], fontSize: 35, color: '#ff82c1', fontWeight: 'bold' }}>Player 2: {getPlayer2}</Text>
 
       <View style={styles.row}>
-        <Pressable style={styles.button}>
+        <Pressable style={styles.button}
+          onPress={() => {
+            checkTurn(0);
+            setTurn(getTurn + 1)
+          }}
+        >
+          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[0]}</Text>
         </Pressable>
-        <Pressable style={styles.button}>
+        <Pressable style={styles.button}
+          onPress={() => {
+            checkTurn(1);
+            setTurn(getTurn + 1)
+          }}
+        >
+          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[1]}</Text>
         </Pressable>
-        <Pressable style={styles.button}>
+
+        <Pressable style={styles.button}
+          onPress={() => {
+            checkTurn(2);
+            setTurn(getTurn + 1)
+          }}
+        >
+          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[2]}</Text>
         </Pressable>
       </View>
 
       <View style={styles.row}>
-        <Pressable style={styles.button}>
+        <Pressable style={styles.button}
+          onPress={() => {
+            checkTurn(3);
+            setTurn(getTurn + 1)
+          }}
+        >
+          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[3]}</Text>
         </Pressable>
-        <Pressable style={styles.button}>
+        <Pressable style={styles.button}
+          onPress={() => {
+            checkTurn(4);
+            setTurn(getTurn + 1)
+          }}
+        >
+          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[4]}</Text>
         </Pressable>
-        <Pressable style={styles.button}>
+
+        <Pressable style={styles.button}
+          onPress={() => {
+            checkTurn(5);
+            setTurn(getTurn + 1)
+          }}
+        >
+          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[5]}</Text>
         </Pressable>
       </View>
 
       <View style={styles.row}>
-        <Pressable style={styles.button}>
+        <Pressable style={styles.button}
+          onPress={() => {
+            checkTurn(6);
+            setTurn(getTurn + 1)
+          }}
+        >
+          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[6]}</Text>
         </Pressable>
-        <Pressable style={styles.button}>
+        <Pressable style={styles.button}
+          onPress={() => {
+            checkTurn(7);
+            setTurn(getTurn + 1)
+          }}
+        >
+          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[7]}</Text>
         </Pressable>
-        <Pressable style={styles.button}>
+
+        <Pressable style={styles.button}
+          onPress={() => {
+            checkTurn(8);
+            setTurn(getTurn + 1)
+          }}
+        >
+          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[8]}</Text>
         </Pressable>
       </View>
-      <Text style={{fontSize: 30, color: '#ffa1d0'}}>Player 1: {getPlayer1}</Text>
+
+      <Text style={{ fontSize: 35, color: '#ff82c1', fontWeight: 'bold' }}>Player 1: {getPlayer1}</Text>
     </View>
   );
 };
@@ -63,10 +131,11 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: '#ffd880',
-    width: '25%',
+    width: 90,
     height: '100%',
-    textAlign: 'center',
-    marginHorizontal: 2 
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 2
   }
 });
 
