f6c93379a7979be54bd403e5f47ffc5661b569f3
Ahsan Khan <imkhanahsan@yahoo.com>
2021-04-15 12:55:07 +0500

    Fifth Commit - Modal View

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Alert, Modal, Pressable, StatusBar, StyleSheet, Text, View } from 'react-native';
 
 var array = ["", "", "", "", "", "", "", "", ""];
 const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   const [getScore1, setScore1] = useState(0);
   const [getScore2, setScore2] = useState(0);
   const [getResult, setResult] = useState("");
@@ -29,25 +30,30 @@ const App = () => {
 
     for (var i = 0; i < sequence.length; i++) {
       const [x, y, z] = sequence[i];
-      if(array[x] != '' && array[y] != '' && array[z] != ''){
-        if (array[x] == array[y] && array[y] == array[z] ) {
+      if (array[x] != '' && array[y] != '' && array[z] != '') {
+        if (array[x] == array[y] && array[y] == array[z]) {
           checkWinner();
-          setResult(array[x]);
           break;
+        } else if (getTurn == 9) {
+          setResult("Draw");
+          setTurn(1);
+          setModalVisible(true);
         }
       }
     }
   }
 
   const checkWinner = () => {
-    if (getResult == "X") {
+    if (getTurn % 2 == 1) {
       setScore1(getScore1 + 1);
-      resetArray();
-      setResult("");
-    } else if (getResult == "O") {
+      setResult("Player 1 Wins");
+      setTurn(1);
+      setModalVisible(true);
+    } else if (getTurn % 2 == 0) {
       setScore2(getScore2 + 1);
-      resetArray();
-      setResult("");
+      setResult("Player 2 Wins");
+      setTurn(1);
+      setModalVisible(true);
     }
   }
 
@@ -57,8 +63,11 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={{ transform: [{ rotate: '180deg' }], fontSize: 35, color: '#ff82c1', fontWeight: 'bold' }}>Player 2: {getScore2}</Text>
 
+      <Text style={{ fontSize: 45, marginVertical: 5, color: '#ff9ecd', fontWeight: 'bold' }}>Tic-Tac-Toe</Text>
+      <Text style={{ transform: [{ rotate: '180deg' }], fontSize: 35, color: '#ff9ecd', fontWeight: 'bold' }}>Player 2: {getScore2}</Text>
+
+      {/* Row 1 */}
       <View style={styles.row}>
         <Pressable style={styles.button}
           onPress={() => {
@@ -66,7 +75,7 @@ const App = () => {
             checkSequence();
           }}
         >
-          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[0]}</Text>
+          <Text style={{ fontSize: 80, color: '#ff9ecd' }}>{array[0]}</Text>
         </Pressable>
         <Pressable style={styles.button}
           onPress={() => {
@@ -74,7 +83,7 @@ const App = () => {
             checkSequence();
           }}
         >
-          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[1]}</Text>
+          <Text style={{ fontSize: 80, color: '#ff9ecd' }}>{array[1]}</Text>
         </Pressable>
 
         <Pressable style={styles.button}
@@ -83,10 +92,11 @@ const App = () => {
             checkSequence();
           }}
         >
-          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[2]}</Text>
+          <Text style={{ fontSize: 80, color: '#ff9ecd' }}>{array[2]}</Text>
         </Pressable>
       </View>
 
+      {/* Row 2 */}
       <View style={styles.row}>
         <Pressable style={styles.button}
           onPress={() => {
@@ -94,7 +104,7 @@ const App = () => {
             checkSequence();
           }}
         >
-          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[3]}</Text>
+          <Text style={{ fontSize: 80, color: '#ff9ecd' }}>{array[3]}</Text>
         </Pressable>
         <Pressable style={styles.button}
           onPress={() => {
@@ -102,7 +112,7 @@ const App = () => {
             checkSequence();
           }}
         >
-          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[4]}</Text>
+          <Text style={{ fontSize: 80, color: '#ff9ecd' }}>{array[4]}</Text>
         </Pressable>
 
         <Pressable style={styles.button}
@@ -111,10 +121,11 @@ const App = () => {
             checkSequence();
           }}
         >
-          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[5]}</Text>
+          <Text style={{ fontSize: 80, color: '#ff9ecd' }}>{array[5]}</Text>
         </Pressable>
       </View>
 
+      {/* Row 3 */}
       <View style={styles.row}>
         <Pressable style={styles.button}
           onPress={() => {
@@ -122,7 +133,7 @@ const App = () => {
             checkSequence();
           }}
         >
-          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[6]}</Text>
+          <Text style={{ fontSize: 80, color: '#ff9ecd' }}>{array[6]}</Text>
         </Pressable>
         <Pressable style={styles.button}
           onPress={() => {
@@ -130,7 +141,7 @@ const App = () => {
             checkSequence();
           }}
         >
-          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[7]}</Text>
+          <Text style={{ fontSize: 80, color: '#ff9ecd' }}>{array[7]}</Text>
         </Pressable>
 
         <Pressable style={styles.button}
@@ -139,14 +150,48 @@ const App = () => {
             checkSequence();
           }}
         >
-          <Text style={{ fontSize: 80, color: '#ffa1d0' }}>{array[8]}</Text>
+          <Text style={{ fontSize: 80, color: '#ff9ecd' }}>{array[8]}</Text>
         </Pressable>
       </View>
 
-      <Text style={{ fontSize: 35, color: '#ff82c1', fontWeight: 'bold' }}>Player 1: {getScore1}</Text>
+      <Text style={{ fontSize: 35, color: '#ff9ecd', fontWeight: 'bold' }}>Player 1: {getScore1}</Text>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>{getResult}</Text>
+            <Pressable
+              style={[styles.buttonModal, styles.buttonClose]}
+              onPress={() => {
+                resetArray();
+                setResult("");
+                setModalVisible(false);
+              }}
+            >
+              <Text style={styles.textStyle}>Play Again</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.buttonModal, styles.buttonClose]}
+              onPress={() => {
+                resetArray();
+                setResult();
+                setScore1(0);
+                setScore2(0);
+                setModalVisible(false);
+              }}
+            >
+              <Text style={styles.textStyle}>Reset Game</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
-};
+}
 
 
 const styles = StyleSheet.create({
@@ -172,6 +217,51 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginHorizontal: 2
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    width: "65%",
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  buttonModal: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginVertical: 5,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 16
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    fontSize: 25
   }
 });
 
